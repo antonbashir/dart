7380a57f650e107a12d802a7831933ec1828d63c
anton.bashirov <anton.sh.local@gmail.com>
2024-08-26 23:49:57 +0400

    fibers

--- sdk/lib/_internal/js_runtime/lib/fiber_patch.dart
+++ sdk/lib/_internal/js_runtime/lib/fiber_patch.dart
@@ -1,3 +1,17 @@
 import "dart:_internal" show patch;
 import "dart:fiber";
 
+@patch
+class Fiber {
+  @patch
+  FiberState get state => _state;
+  var _state = FiberState.created;
+  @patch
+  Fiber({required int size, required void Function() entry, required String name}) : this.name = name {}
+  @patch
+  void start() {}
+  @patch
+  void transfer(Fiber to) {}
+  @patch
+  void fork(Fiber to) {}
+}
