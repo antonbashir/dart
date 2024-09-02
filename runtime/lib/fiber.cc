9160f9e10e2bda9669bc0529170452d9ce0ea06d
anton.bashirov <anton.sh.local@gmail.com>
2024-09-03 01:34:13 +0400

    fibers

--- runtime/lib/fiber.cc
+++ runtime/lib/fiber.cc
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#include <sys/mman.h>
 #include "vm/bootstrap_natives.h"
 
 #include "vm/compiler/runtime_api.h"
@@ -12,7 +13,7 @@ namespace dart {
 DEFINE_NATIVE_ENTRY(Coroutine_factory, 0, 3) {
   GET_NON_NULL_NATIVE_ARGUMENT(Smi, size, arguments->NativeArgAt(1));
   GET_NON_NULL_NATIVE_ARGUMENT(Closure, entry, arguments->NativeArgAt(2));
-  uword stack = VirtualMemory::Allocate(size.Value() * kWordSize, false, false, "coroutine-stack")->end();
-  return Coroutine::New((void**)stack , size.Value(), Function::Handle(entry.function()).ptr());
+  void** stack_base = (void**)((uintptr_t)mmap(0, size.Value() * sizeof(word), PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) + size.Value());
+  return Coroutine::New(stack_base , size.Value(), Function::Handle(entry.function()).ptr());
 }
 }  // namespace dart
