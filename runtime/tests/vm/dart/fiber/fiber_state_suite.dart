df648e77eaae5d662cc36c2dfc3049f6634377f7
anton.bashirov <anton.sh.local@gmail.com>
2024-11-11 11:18:21 +0400

    tests

--- runtime/tests/vm/dart/fiber/fiber_state_suite.dart
+++ runtime/tests/vm/dart/fiber/fiber_state_suite.dart
@@ -10,6 +10,26 @@ final tests = [
 
 void testRegistry() {
   Fiber.launch(() => Expect.equals(2, Fiber.registry.length));
+  Expect.equals(0, Fiber.registry.length);
+  Fiber.launch(() {
+    for (var i = 0; i < 32; i++) {
+      Fiber.spawn(Fiber.reschedule);
+    }
+    Expect.equals(2 + 32, Fiber.registry.length);
+  });
+  Expect.equals(0, Fiber.registry.length);
+
+  late var currentHash;
+  Fiber.launch(() {
+    for (var i = 0; i < 65; i++) {
+      Fiber.spawn(Fiber.reschedule);
+    }
+    currentHash = Fiber.registry.hashCode;
+    for (var i = 0; i < 65; i++) {
+      Fiber.reschedule();
+    }
+    Expect.notEquals(currentHash, Fiber.registry.hashCode);
+  });
 }
 
 void testStateLink() {}
